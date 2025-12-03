cb3a882e643efa74b6e18016429628eab30654a6
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-12-03 11:55:28 +0100

    fixes

--- src/main/java/com/leclowndu93150/tinkers_old_guns/TinkersOldGuns.java
+++ src/main/java/com/leclowndu93150/tinkers_old_guns/TinkersOldGuns.java
@@ -1,43 +1,22 @@
 package com.leclowndu93150.tinkers_old_guns;
 
 import com.leclowndu93150.tinkers_old_guns.common.TinkersGunStats;
+import com.leclowndu93150.tinkers_old_guns.config.TOGConfig;
 import com.leclowndu93150.tinkers_old_guns.registry.TinkersGunModifiers;
 import com.leclowndu93150.tinkers_old_guns.registry.TinkersGunParts;
 import com.leclowndu93150.tinkers_old_guns.registry.TinkersGunTabs;
 import com.leclowndu93150.tinkers_old_guns.registry.TinkersGunTools;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-import slimeknights.mantle.registration.deferred.ItemDeferredRegister;
-import slimeknights.mantle.registration.object.ItemObject;
-import slimeknights.tconstruct.common.registration.CastItemObject;
-import slimeknights.tconstruct.common.registration.ItemDeferredRegisterExtension;
 import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 
 @Mod(TinkersOldGuns.MODID)
